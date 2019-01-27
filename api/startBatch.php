82430629cd181dacfc320532006baa7cba41f27f
Anmol Saxena <anmol.saxena10@gmail.com>
2019-01-27 21:37:56 +0530

    complete

--- api/startBatch.php
+++ api/startBatch.php
@@ -5,7 +5,7 @@
 			$sql = "INSERT INTO batch (time_started, ph, tds, color) VALUES (?, ?, ?, ?) RETURNING batch_id;";
 			$stmt = $database_handler->prepare($sql);
 			$stmt->execute(array($_POST['startTime'], $_POST['ph'], $_POST['tds'], $_POST['color']));
-			print_r(array($_POST['startTime'], $_POST['ph'], $_POST['tds'], $_POST['color']));
+			// print_r(array($_POST['startTime'], $_POST['ph'], $_POST['tds'], $_POST['color']));
 			echo $stmt->fetch(PDO::FETCH_ASSOC)['batch_id'];
 
 		}

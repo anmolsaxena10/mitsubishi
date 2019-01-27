381c614bd769b5a34519f0fcf8f07369d5e24f22
Anmol Saxena <anmol.saxena10@gmail.com>
2019-01-27 20:16:01 +0530

    complete

--- api/startBatch.php
+++ api/startBatch.php
@@ -1,17 +1,18 @@
 <?php
 	if(isset($_POST['startTime']) && isset($_POST['color']) && isset($_POST['ph']) && isset($_POST['tds'])){
 		try{
+			echo "1   ";
 			require_once('../connection.php');
 			$sql = "INSERT INTO batch (time_started, ph, tds, color) VALUES (?, ?, ?, ?) RETURNING batch_id;";
 			$stmt = $database_handler->prepare($sql);
 			$stmt->execute(array($_POST['startTime'], $_POST['ph'], $_POST['tds'], $_POST['color']));
-
+			echo "2    ";
 			echo $stmt->fetch(PDO::FETCH_ASSOC)['batch_id'];
+
 		}
 		catch(ErrorException $e){
 			echo $e->getMessage();
 		}
-		echo "asdsad";
 	}
 	else{
 		echo"failed";
